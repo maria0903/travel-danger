cedc55b2e476ae9b7d36abb83ca99520ca3adb11
maria0903 <marian.mhappy2000[gmail.com>
2023-08-14 08:15:06 +0900

    hi 2

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,20 @@
+import React from "react"
+import { Select, Space } from "antd"
+
 export default function Dashboard(){
   return (
     <body>
       <header>
         <h1 className="title">Map</h1>
+        <div className="language-wrap">
+        <Select className="language"
+        defaultValue="Korean"
+        options={[
+          { value:'Japanese', label:'Japanese'},
+          { value:'Korean', label:'Korean'},
+        ]}
+        />
+        </div>
       </header>
       <footer>
         <p><small>copyright 2023.maria.wonseok.All rights reserved</small></p>
